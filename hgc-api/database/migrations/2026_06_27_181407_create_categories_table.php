82810eb61ce2269de3386b50967768289f53b3a6
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-06-27 22:45:03 +0430

    category mvc created

---
+++ hgc-api/database/migrations/2026_06_27_181407_create_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+             $table->id();
+            $table->string('slug', 50)->unique();
+            $table->string('name_en', 100);
+            $table->string('name_dari', 100)->nullable();
+            $table->string('name_pashto', 100)->nullable();
+            $table->text('description_en')->nullable();
+            $table->text('description_dari')->nullable();
+            $table->text('description_pashto')->nullable();
+            $table->string('icon_name', 50)->nullable();
+            $table->string('image_url', 255)->nullable();
+            $table->enum('type', ['product', 'project', 'both'])->default('product');
+            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('set null');
+            $table->boolean('is_active')->default(true);
+            $table->integer('sort_order')->default(0);
+            $table->timestamps();
+            
+            $table->index('slug');
+            $table->index('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
