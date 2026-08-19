c1b48a04e633a5451553af98cc1bd0c172dea47f
Abdullah Hussaini <h.abdullah1394@gmail.com>
2026-08-19 21:45:04 +0430

    Update cors.php

--- hgc-api/config/cors.php
+++ hgc-api/config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'http://localhost:3000',    // Next.js dev
-        'https://yourdomain.com',   // Production
+        'https://hgc.af',   // Production
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
